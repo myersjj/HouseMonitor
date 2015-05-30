ada86a2b7e2faf3af7bd646c7f9066da1102e292
myersjj <myersjj@gmail.com>
2015-05-30 11:50:20 -0700

    updates /cleanup

---
+++ HouseReceiverRH/HouseReceiverRH/.ino.cpp
@@ -0,0 +1,17 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2015-03-29 09:37:36
+
+#include "Arduino.h"
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+void blink(int n) ;
+void setup() ;
+void processConfig(char* msg) ;
+void loop() ;
+void serialEvent() ;
+
+
+#include "sensorReceiver.ino"
--- HouseSensor/HouseSensor/.ino.cpp
+++ HouseSensor/HouseSensor/.ino.cpp
@@ -1,20 +1,36 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-03-16 14:08:57
+//This file has been generated on 2015-05-09 11:28:53
 
 #include "Arduino.h"
+#include <U8glib.h>
 #include <RF24Network.h>
 #include <RF24.h>
 #include <SPI.h>
-#include <printf.h>
-#include <JeeLib.h>
-#include <DHT.h>
 #include <ArduinoJson.h>
+#include <EEPROM.h>
+#include <printf.h>
+#include "DebugUtils.h"
+#include <Wire.h>
+#include <FiniteStateMachine.h>
+#include <DHT22.h>
+#include <HTU21D.h>
+void oled_log(const char* s, uint8_t line, uint8_t x, bool clear, bool update) ;
+long readVcc() ;
+boolean sendMessage(char *buffer, char msgtype) ;
 void setup() ;
-int ftoa(char *a, float f)  		;
-boolean sendMessage(char *buffer) ;
+void doInit() ;
+void doConfig() ;
+void doConfigRetry() ;
+void doConfigWait() ;
+void doWaitAck() ;
+int ftoa(char *a, float f)  ;
+void updateTOD() ;
 void loop() ;
+void doSleep() ;
+void checkNetwork() ;
+void doReadSensors() ;
 
 
-#include "sensorController.ino"
+#include "HouseSensor.ino"
